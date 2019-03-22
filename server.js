4e39ed29298004d4df24f0c8e03cd2b829f3a9d9
Rod Aragon <rnaragon001@yahoo.com>
2019-03-23 00:02:40 +0800

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-convex-61122',
       user : 'rnaragon',
       password : 'password',
       database : 'facerecognitionbrain'
