ec5c1840cff9f83d298add73150d2299f5b32cf9
Rod Aragon <rnaragon001@yahoo.com>
2019-03-22 23:05:42 +0800

    changing package.json start script from nodemon to node server.js

--- server.js
+++ server.js
@@ -61,7 +61,8 @@ app.use(cors());
 
 
 
-app.get('/', (req, res) => { res.send(database.users) })
+// app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send("it is working!") })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
 
