1e9668f7bae53723a6dac010c3d0bcc3732a977e
Rod Aragon <rnaragon001@yahoo.com>
2019-03-22 21:31:18 +0800

    initial commit

---
+++ controllers/image.js
@@ -0,0 +1,48 @@
+const clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'b80cfe04bcb040dca9bdfbe2f2f48332'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+   
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+
+
+
+    // let found = false;
+    // database.users.forEach(user => {
+    //     if (user.id === id) {
+    //         found = true;
+    //         user.entries++;
+    //         return res.json(user.entries);
+    //     } 
+    // })
+    // if (!found) {
+    //     res.status(400).json('not found');
+    // }
+    // console.log('found:', found); 
+    
+    db('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
---
+++ controllers/profile.js
@@ -0,0 +1,37 @@
+const handleProfileGet = (req, res, db) => {
+    const { id } = req.params;
+    // let found = false;
+    
+    
+    // database.users.forEach(user => {
+    //     if (user.id === id) {
+    //         found = true;
+    //         return res.json(user);
+    //     } 
+    // })
+    
+    // db.select('*').from('users').where({
+    //     id:id
+    // })
+
+    db.select('*').from('users').where({id})
+        .then(user =>{
+            if(user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json('not found');
+            }
+            
+        })
+        .catch(err => {
+            res.status(400).json('error getting user');
+        })
+
+    // if (!found) {
+    //     res.status(400).json('not found');
+    // }
+}
+
+module.exports = {
+    handleProfileGet
+}
